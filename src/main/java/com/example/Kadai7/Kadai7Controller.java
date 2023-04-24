6596d5512ed59840a0e5ba6d879563e7a3312133
Tom-Shimoyama <125033423+Tom-Shimoyama@users.noreply.github.com>
2023-04-24 21:34:12 +0900

    delete処理とバリデーションの追加をしました。

--- src/main/java/com/example/Kadai7/Kadai7Controller.java
+++ src/main/java/com/example/Kadai7/Kadai7Controller.java
@@ -1,7 +1,13 @@
 package com.example.Kadai7;
 
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -9,12 +15,15 @@ import java.net.URI;
 import java.util.List;
 import java.util.Map;
 
+@Validated
 @RestController
 public class Kadai7Controller {
 
     @GetMapping("/names")
-    public List<String> getNames() {
-        return List.of("Shimoyama", "Nakano");
+    @NotEmpty
+    @Valid
+    public List<@NotBlank String> getNames() {
+        return List.of("Shimoyama", "Nakano","");
     }
 
     @PostMapping("/names")
@@ -28,8 +37,16 @@ public class Kadai7Controller {
     }
 
     @PatchMapping("/names/{id}")
-    public ResponseEntity<Map<String, String>> update(@PathVariable("id") int id, @RequestBody UpdateForm form) {
-        // 更新処理は 省略
+    public ResponseEntity<Map<String, String>> update(@PathVariable("id") int id,
+                                                      @RequestBody UpdateForm form) {
+        // 更新処理は省略
         return ResponseEntity.ok(Map.of("message", "name successfully updated"));
     }
+
+    @DeleteMapping("names/{id}")
+    public ResponseEntity<Map<String,String>> delete(@PathVariable("id") int id) {
+        //削除処理は省略
+        return  ResponseEntity.ok(Map.of("massage","name successfully deleted"));
+    }
+
 }
